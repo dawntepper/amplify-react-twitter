4b602943cfa886085e55cf7e7cebe9723d69bf9b
dawntepper <dawn.dtepdc@gmail.com>
2024-07-03 22:20:27 -0400

    updates to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -6,6 +8,10 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -16,14 +22,24 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
+    
+    <Authenticator>
+      {({ signOut }) => (
+
     <main>
+    
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)}
+		 key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -34,6 +50,9 @@ function App() {
         </a>
       </div>
     </main>
+    
+      )}
+    </Authenticator>
   );
 }
 
